464abeef8c5ecf18860800188cc6424bda2c0220
richardk88 <richardk88@hotmail.com>
2017-08-11 09:46:45 -0400

    added schema db

---
+++ db/schema.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+const stepSchema = mongoose.Schema({
+    name: String,
+    description: String,
+    image: String,
+    video: String,
+    createdAt: Date,
+    updatedAt: Date
+});
+
+const projectSchema = mongoose.Schema({
+    name: String,
+    description: String,
+    category: String,
+    author: String,
+    createdAt: Date,
+    updatedAt: Date,
+    steps: [stepSchema]
+});
+
+const userSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    userName: String,
+    email: String,
+    createdAt: Date,
+    updatedAt: Date,
+    projects: [projectSchema]
+})
+
+stepSchema.pre('save', (next) => {
+    now = new Date();
+    this.updatedAt = now;
+    if(!this.createdAt) {
+        this.createdAt = now;
+    }
+    next();
+});
+
+projectSchema.pre('save', (next) => {
+    now = new Date();
+    this.updatedAt = now;
+    if(!this.createdAt) {
+        this.createdAt = now;
+    }
+    next();
+});
+
+userSchema.pre('save', (next) => {
+    now = new Date();
+    this.updatedAt = now;
+    if(!this.createdAt) {
+        this.createdAt = now;
+    }
+    next();
+});
+
+const Step = mongoose.model('Step', stepSchema);
+const Project = mongoose.model('Project', projectSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    Step, Project, User
+}
