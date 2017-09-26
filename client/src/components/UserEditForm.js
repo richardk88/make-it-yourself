4ecded9a0ae4214af813efea5a4f686995e1e87d
richardk88 <richardk88@hotmail.com>
2017-09-26 18:12:50 -0400

    styled user edit form

--- client/src/components/UserEditForm.js
+++ client/src/components/UserEditForm.js
@@ -52,27 +52,28 @@ class UserEditForm extends Component {
         } else {
             return (
                 <div>
-                    <h1>Edit User Dashboard</h1>
-                    <form onSubmit={this._editUser}>
-                        <div>
-                            <input name="userName" type="text" value={this.state.user.userName} onChange={this._handleEditUpdate} placeholder="User Name"/>
-                        </div>
-                        <div>
-                            <input name="firstName" type="text" value={this.state.user.firstName}  onChange={this._handleEditUpdate} placeholder="First Name"/>
-                        </div>
-                        <div>       
-                            <input name="lastName" type="text" value={this.state.user.lastName} onChange={this._handleEditUpdate} placeholder="Last Name"/>
-                        </div>
-                        <div>
-                            <input name="email" type="text" value={this.state.user.email} onChange={this._handleEditUpdate} placeholder="E-mail"/>
-                        </div>
-                        <div>
-                            <input name="blurb" type="text" value={this.state.user.blurb} onChange={this._handleEditUpdate} placeholder="Brief Intro"/>
-                        </div>
-                            <input type='submit'/>
-                    </form>
-                    <br />
-                    <Link to={`/user/${this.props.match.params.userId}`}>Go back</Link>
+                    <h1 className='title'>Edit User Dashboard</h1>
+                    <div className='editForm boxShadow'>
+                        <form onSubmit={this._editUser} >
+                            <div>
+                                <input name="userName" type="text" value={this.state.user.userName} onChange={this._handleEditUpdate} placeholder="User Name"/>
+                            </div>
+                            <div>
+                                <input name="firstName" type="text" value={this.state.user.firstName}  onChange={this._handleEditUpdate} placeholder="First Name"/>
+                            </div>
+                            <div>       
+                                <input name="lastName" type="text" value={this.state.user.lastName} onChange={this._handleEditUpdate} placeholder="Last Name"/>
+                            </div>
+                            <div>
+                                <input name="email" type="text" value={this.state.user.email} onChange={this._handleEditUpdate} placeholder="E-mail"/>
+                            </div>
+                            <div>
+                                <input name="blurb" type="text" value={this.state.user.blurb} onChange={this._handleEditUpdate} placeholder="Brief Intro"/>
+                            </div>
+                                <input type='submit'className='btnColor'/>
+                        </form>
+                        <Link to={`/user/${this.props.match.params.userId}`} className='backBtn'>Go back</Link>
+                    </div>
                 </div>
             );
         }
